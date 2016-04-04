e28c117c7cc35051847d06aef01c18122f12b35f
Matt Hayes <matt@mysterycommand.com>
2016-04-03 21:09:02 -0700

    refactor(deploy): attempt to deploy PRs to staging

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,24 @@ rm -rf deploy || exit 0;
 git clone --quiet --branch=master --single-branch "https://${GH_TOKEN}@${GH_REPOSITORY}" deploy > /dev/null 2>&1
 cd deploy
 
-git rm -r .
-cp -R ../public/* .
+if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
 
-git config user.name "Travis CI"
-git config user.email "travis@travis-ci.org"
-git config push.default simple
+    if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
+        DEPLOY_DIR = ./staging/${TRAVIS_PULL_REQUEST}
+        mkdir -p ${DEPLOY_DIR}
+    else
+        DEPLOY_DIR = .
+    fi
 
-git add .
-git commit --allow-empty -am "Travis buld #${TRAVIS_BUILD_NUMBER} - `date +\"%D %T\"` [ci skip]"
-git push
+    git rm -r ${DEPLOY_DIR}
+    cp -R ../public/* ${DEPLOY_DIR}
+
+    git config user.name "Travis CI"
+    git config user.email "travis@travis-ci.org"
+    git config push.default simple
+
+    git add .
+    git commit --allow-empty -am "Travis buld #${TRAVIS_BUILD_NUMBER} - `date +\"%D %T\"` [ci skip]"
+    git push
+
+fi
