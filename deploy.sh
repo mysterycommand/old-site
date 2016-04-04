d610cd55895ce1fe1ed97b449ad6f02f8d990ce5
Matt Hayes <matt@mysterycommand.com>
2016-04-03 21:13:15 -0700

    refactor(deploy): there must be a better way to do this kind of testing

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ rm -rf deploy || exit 0;
 git clone --quiet --branch=master --single-branch "https://${GH_TOKEN}@${GH_REPOSITORY}" deploy > /dev/null 2>&1
 cd deploy
 
+echo $TRAVIS_BRANCH
+echo $TRAVIS_PULL_REQUEST
+
 if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
 
     if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
