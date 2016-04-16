dca636ef1bf76c999696e55d2e838b2e5477caf8
Matt Hayes <matt@mysterycommand.com>
2016-04-16 09:21:50 -0700

    refactor(deploy.js): almost working, but now I don't like it

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const exec = require('child_process').exec;
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 const mkdirp = require('mkdirp');
@@ -15,11 +15,19 @@ const glob = require('glob');
 const CLONE_URL = process.env.TRAVIS
     ? `https://${process.env.GH_TOKEN}@github.com/${process.env.TRAVIS_REPO_SLUG}`
     : 'git@github.com:mysterycommand/mysterycommand.github.io.git';
+const DEPLOY_DIR = './deploy';
+const PUBLIC_DIR = './public';
 
-function gitClone(url) {
+const tpr = process.env.TRAVIS_PULL_REQUEST;
+console.log('TRAVIS_PULL_REQUEST:', tpr, tpr === 'false');
+const dir = tpr && tpr !== 'false'
+    ? `${DEPLOY_DIR}/staging/${process.env.TRAVIS_PULL_REQUEST}`
+    : `${DEPLOY_DIR}`;
+
+function gitClone() {
     console.log('gitClone');
     return new Promise((resolve, reject) => {
-        const cloneCmd = `git clone -qb master --single-branch ${url} deploy > /dev/null 2>&1`;
+        const cloneCmd = `git clone -qb master --single-branch ${CLONE_URL} ${DEPLOY_DIR} > /dev/null 2>&1`;
 
         exec(cloneCmd, null, (error, stdout, stderr) => {
             if (error) {
@@ -33,12 +41,6 @@ function gitClone(url) {
 
 function makeDeploy() {
     console.log('makeDeploy');
-    const tpr = process.env.TRAVIS_PULL_REQUEST;
-    console.log('TRAVIS_PULL_REQUEST:', tpr, tpr === 'false');
-    const dir = tpr && tpr !== 'false'
-        ? `./deploy/staging/${process.env.TRAVIS_PULL_REQUEST}`
-        : './deploy';
-
     return new Promise((resolve, reject) => {
         mkdirp(dir, error => {
             if (error) {
@@ -50,12 +52,12 @@ function makeDeploy() {
     });
 }
 
-function globFiles(dir) {
-    console.log('globFiles');
+function globDeploy(dir) {
+    console.log('globDeploy');
     return new Promise((resolve, reject) => {
         glob(`${dir}/!(staging)`, (error, files) => {
             if (error) {
-                console.log('globFiles:error', error);
+                console.log('globDeploy:error', error);
                 reject(error);
             }
             resolve(files);
@@ -66,28 +68,55 @@ function globFiles(dir) {
 function cleanDeploy(files) {
     console.log('cleanDeploy');
     if (!files.length) {
-        return Promise.resolve();
+        return Promise.resolve(`${PUBLIC_DIR}`);
     }
 
-    files = files.map(file => path.relative('./deploy', file));
+    files = files.map(file => path.relative(`${DEPLOY_DIR}`, file));
 
     return new Promise((resolve, reject) => {
         const rmCmd = `git rm -r --ignore-unmatch ${files.join(' ')}`;
 
-        exec(rmCmd, { cwd: './deploy' }, (error, stdout, stderr) => {
+        exec(rmCmd, { cwd: `${DEPLOY_DIR}` }, (error, stdout, stderr) => {
             if (error) {
                 console.log('cleanDeploy:error', error);
                 reject(error);
             }
-            resolve();
+            resolve(`${PUBLIC_DIR}`);
         });
     });
 }
 
-gitClone(CLONE_URL)
+function globPublic(dir) {
+    console.log('globPublic');
+    return new Promise((resolve, reject) => {
+        glob(`${dir}/*`, (error, files) => {
+            if (error) {
+                console.log('globPublic:error', error);
+                reject(error);
+            }
+            resolve(files);
+        });
+    });
+}
+
+function copyPublic(files) {
+    console.log('copyPublic');
+    console.log(files);
+    return Promise.resolve();
+    // return Promise.all(files.map((file) => {
+    //     return new Promise((resolve, reject) => {
+    //         fs.createReadStream(file)
+    //             .pipe(fs.createWriteStream())
+    //     })
+    // }));
+}
+
+gitClone()
     .then(makeDeploy)
-    .then(globFiles)
+    .then(globDeploy)
     .then(cleanDeploy)
+    .then(globPublic)
+    .then(copyPublic)
     .catch(error => {
         console.log('error', error);
         process.exit(1);
