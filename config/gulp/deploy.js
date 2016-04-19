3d69f1dbd897605e4b08f1891af852fd3eb002c2
Travis CI <travis@travis-ci.org>
2016-04-19 00:27:40 -0700

    refactor(deploy): break the deploy steps into commands and options

--- config/gulp/deploy.js
+++ config/gulp/deploy.js
@@ -1,9 +1,6 @@
 import { exec } from 'child_process';
 import path from 'path';
-
 import glob from 'glob';
-import gulp from 'gulp';
-import mkdirp from 'mkdirp';
 
 const {
     GH_REPO,
@@ -27,92 +24,47 @@ const outDir = isPr
     ? `${deployDir}/staging/${TRAVIS_PULL_REQUEST}`
     : `${deployDir}`;
 
-// function gitClone(url) {
-//     return new Promise((resolve, reject) => {
-//         exec(`git clone -qb master --single-branch ${url} deploy > /dev/null 2>&1`, null, (error, stdout, stderr) => {
-//             if (error) {
-//                 reject(error);
-//             }
-//             console.log(`git clone -qb master --single-branch ${url} deploy > /dev/null 2>&1`);
-//             resolve(stdout);
-//         });
-//     });
-// }
-
-// function makeOutDir() {
-//     return new Promise((resolve, reject) => {
-//         mkdirp(outDir, (error) => {
-//             if (error) {
-//                 reject(error);
-//             }
-//             resolve();
-//         });
-//     });
-// }
-
-// function globOldFiles() {
-//     return new Promise((resolve, reject) => {
-//         glob(`${outDir}/!(staging)`, null, (error, matches) => {
-//             if (error) {
-//                 reject(error);
-//             }
-//             resolve(matches.join(' '));
-//         });
-//     });
-// }
-
-// function gitRmOldFiles(files) {
-//     if (!files) {
-//         return Promise.resolve();
-//     }
-
-//     return new Promise((resolve, reject) => {
-//         exec(`git rm -r --ignore-unmatch ${files}`, { cwd: deployDir }, (error, stdout, stderr) => {
-//             if (error) {
-//                 reject(error);
-//             }
-//             console.log(`git rm -r --ignore-unmatch ${files}`);
-//             resolve(stdout);
-//         });
-//     });
-// }
-
-// function copyPublic() {
-//     return new Promise((resolve, reject) => {
-//         exec(`cp -R ${publicDir}/* ${outDir}`, null, (error, stdout, stderr) => {
-//             if (error) {
-//                 reject(error);
-//             }
-//             console.log(`cp -R ${publicDir}/* ${outDir}`);
-//             resolve(stdout);
-//         });
-//     });
-// }
+const steps = [{
+    cmd: `git clone -qb master --single-branch ${url} deploy > /dev/null 2>&1`,
+    opts: null,
+},{
+    cmd: `mkdir -p ${outDir}`,
+    opts: null,
+},{
+    cmd: `git rm -r --ignore-unmatch ${glob.sync(`${outDir}/!(staging)`).join() || '+'}`,
+    opts: null,
+},{
+    cmd: `cp -R ${publicDir}/* ${outDir}`,
+    opts: null,
+},{
+    cmd: `git config user.name "Travis CI"`,
+    opts: { cwd: deployDir },
+},{
+    cmd: `git config user.email "travis@travis-ci.org"`,
+    opts: { cwd: deployDir },
+},{
+    cmd: `git config push.default simple`,
+    opts: { cwd: deployDir },
+},{
+    cmd: `git add .`,
+    opts: { cwd: deployDir },
+},{
+    cmd: `git commit --allow-empty -am "Travis buld #${TRAVIS_BUILD_NUMBER} - \`date +\"%D %T\"\` [ci skip]"`,
+    opts: { cwd: deployDir },
+},{
+    cmd: `git push`,
+    opts: { cwd: deployDir },
+}];
 
 export default () => {
-    return [
-        `git clone -qb master --single-branch ${url} deploy > /dev/null 2>&1`,
-        `cd deploy`,
-
-        `mkdir -p ${outDir}`,
-        `git rm -r --ignore-unmatch ${glob.sync(`${outDir}/!(staging)`).join() || '+'}`,
-        `cp -R ${publicDir}/* ${outDir}`,
-
-        `git config user.name "Travis CI"`,
-        `git config user.email "travis@travis-ci.org"`,
-        `git config push.default simple`,
-
-        `git add .`,
-        `git commit --allow-empty -am "Travis buld #${TRAVIS_BUILD_NUMBER} - \`date +\"%D %T\"\` [ci skip]"`,
-        `git push`,
-    ].reduce((commands, command) => {
-        return commands.then(() => {
+    return steps.reduce((cmds, {cmd, opts}) => {
+        return cmds.then(() => {
             return new Promise((resolve, reject) => {
-                exec(command, null, (error, stdout, stderr) => {
+                exec(cmd, opts, (error, stdout, stderr) => {
                     if (error) {
                         reject(error);
                     }
-                    console.log(command);
+                    console.log(cmd);
                     resolve(stdout);
                 });
             });
