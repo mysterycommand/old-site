9c9815609b94fdc287fe1bdd8410a747e5f57d92
Matt Hayes <matt@mysterycommand.com>
2016-04-18 23:53:00 -0700

    refactor(gulp): create a deploy task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,12 @@
 import gulp from 'gulp';
-import ghtmlmin from 'gulp-htmlmin';
 
-import html from './config/gulp/html';
 import copy from './config/gulp/copy';
+import deploy from './config/gulp/deploy';
+import html from './config/gulp/html';
 
-gulp.task('html', html);
 gulp.task('copy', copy);
+gulp.task('deploy', deploy);
+gulp.task('html', html);
 
 gulp.task('default', [
     'copy',
